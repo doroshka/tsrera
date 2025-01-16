17985517e665be1bcb0e8ad8721bc36372fcb88a
doroshka <doroshka@gmail.com>
2025-01-16 10:47:04 +0200

    Set server URL as env variable

--- reqapi.py
+++ reqapi.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import time
 import json
 import requests
@@ -6,7 +7,7 @@ def getData(id):
     r = dict()
     r['is_ok'] = 1
 
-    resp = requests.get('http://38.105.232.175:3001/api/0b06d7a7-a049-4739-be8e-8454ae90d638', params={'id': id})
+    resp = requests.get(st.secrets['server'], params={'id': id})
     r['status_code'] = resp.status_code
     if resp.status_code != requests.codes.ok:
        r['is_ok'] = 0
